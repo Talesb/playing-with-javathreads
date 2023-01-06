6708e0a0208b7b3fd480b45f89435493bf2ce6c5
talesb <tales.batista@temasistemas.com.br>
2023-01-05 23:36:10 -0300

    First commit

---
+++ src/com/talesb/playingthreads/subscriptions/Main.java
@@ -0,0 +1,19 @@
+package com.talesb.playingthreads.subscriptions;
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		
+		String nameToSearch = "Dan";
+		Thread authorsThread = new Thread(new SearchNameTask("autores.txt",nameToSearch ));
+		Thread subscriptionsThread = new Thread(new SearchNameTask("assinaturas1.txt",nameToSearch ));
+		Thread subscriptionsThread2 = new Thread(new SearchNameTask("assinaturas2.txt",nameToSearch ));
+		
+		authorsThread.start();
+		subscriptionsThread.start();
+		subscriptionsThread2.start();
+		
+	}
+
+}
---
+++ src/com/talesb/playingthreads/subscriptions/SearchNameTask.java
@@ -0,0 +1,45 @@
+package com.talesb.playingthreads.subscriptions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SearchNameTask implements Runnable {
+
+	private String fileName;
+	private String name;
+
+	public SearchNameTask(String fileName, String name) {
+		this.fileName = fileName;
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+
+		try {
+
+			Scanner scanner = new Scanner(new File(fileName));
+			int lineNumber = 1;
+
+			while (scanner.hasNext()) {
+
+				String line = scanner.next();
+
+				if (line.toLowerCase().contains(name.toLowerCase())) {
+					System.out.println(fileName + " - " + lineNumber + " - " + line);
+
+				}
+				lineNumber++;
+
+			}
+
+			scanner.close();
+
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+}
