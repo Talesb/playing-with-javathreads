0572e0d7373e1df9aca66e77b5945ddb17300fe5
talesb <tales.batista@temasistemas.com.br>
2023-01-06 00:08:02 -0300

    Adding synchronized

--- src/com/talesb/playingthreads/bathroomexample/Bathroom.java
+++ src/com/talesb/playingthreads/bathroomexample/Bathroom.java
@@ -2,40 +2,50 @@ package com.talesb.playingthreads.bathroomexample;
 
 public class Bathroom {
 
+ 
+	
+	
+	
 	public void doTheNumber1() {
 
-	    String name = Thread.currentThread().getName();
-		
-		System.out.println(name+" entering the bathroom");
-		System.out.println(name+" doing something fast");
+		String name = Thread.currentThread().getName();
 
-		try {
-			Thread.sleep(8000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+ 		synchronized (this) {
+			System.out.println(name + " entering the bathroom");
+			System.out.println(name + " doing something fast");
 
-		System.out.println(name+" flushing");
-		System.out.println(name+" washing hands");
-		System.out.println(name+" leaving bathroom");
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println(name + " flushing");
+			System.out.println(name + " washing hands");
+			System.out.println(name + " leaving bathroom");
+ 		}
+
 	}
 
 	public void doTheNumber2() {
-		
+
 		String name = Thread.currentThread().getName();
-		
-		System.out.println(name+" entering the bathroom");
-		System.out.println(name+" doing something slow");
 
-		try {
-			Thread.sleep(15000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+ 		synchronized (this) {
+
+			System.out.println(name + " entering the bathroom");
+			System.out.println(name + " doing something slow");
+
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println(name + " flushing");
+			System.out.println(name + " washing hands");
+			System.out.println(name + " leaving bathroom");
 		}
-
-		System.out.println(name+" flushing");
-		System.out.println(name+" washing hands");
-		System.out.println(name+" leaving bathroom");
-	}
+ 	}
 
 }
--- src/com/talesb/playingthreads/bathroomexample/Main.java
+++ src/com/talesb/playingthreads/bathroomexample/Main.java
@@ -6,11 +6,15 @@ public class Main {
 
 		Bathroom bathroom = new Bathroom();
 
-		Thread ghest1 = new Thread(() -> bathroom.doTheNumber1(), "John");
-		Thread ghest2 = new Thread(() -> bathroom.doTheNumber2(), "Peter");
+		Thread ghest1 = new Thread(() -> bathroom.doTheNumber1(), "Mike");
+		Thread ghest2 = new Thread(() -> bathroom.doTheNumber2(), "Carl");
+		Thread ghest3 = new Thread(() -> bathroom.doTheNumber1(), "John");
+		Thread ghest4 = new Thread(() -> bathroom.doTheNumber2(), "Peter");
 		
 		ghest1.start();
 		ghest2.start();
-
+		ghest3.start();
+		ghest4.start();
+		
 	}
 }
